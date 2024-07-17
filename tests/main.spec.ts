3f68c6f39ac32173f7f6b67ada43022015befea8
KGolanski <konrad.golanski@icloud.com>
2024-07-17 19:41:32 +0200

    Setup Playwright

---
+++ tests/main.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test';
+
+test('should average daily temperature be over 22 degrees in Cracow everyday for next 3 days from Today', async ({ page }) => {
+
+});
+
+test('should average daily temperature be below 42 degrees in Poznan on next Wednesday', async ({ page }) => {
+
+});
+
+test('should not average daily temperature differ more than 5 degrees for Szczecin and Cracow on next Monday', async ({ page }) => {
+
+});
+
+test('should average daily temperature for Cracow, Poznań and Szczecin be over 19 degrees on next Monday', async ({ page }) => {
+
+});
